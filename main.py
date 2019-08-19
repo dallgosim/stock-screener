e854644b5137d1985fe7400517e950e0eab8a741
sangkwon <andy.sangkwon@gmail.com>
2019-08-20 01:38:07 +0900

    change timezone utc to asia/seoul

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ from apscheduler.schedulers.background import BackgroundScheduler
 from scheduler import scheduler
 
 # scheduler
-sched = BackgroundScheduler()
+sched = BackgroundScheduler({'apscheduler.timezone': 'Asia/Seoul'})
 @sched.scheduled_job('cron', hour='9')
 def timed_job():
     scheduler.run_screener()
@@ -24,10 +24,11 @@ app = Flask(__name__)
 
 @app.route('/metric_studio', methods=['GET', 'POST'])
 def hello_world():
+    today = datetime.date.today().strftime('%Y-%m-%d')
     with open('output.pkl', 'rb') as f:
         recomm_df = pickle.load(f)
-    return render_template('metric_studio/index.html', recomm_df=recomm_df.to_html())
+    return render_template('metric_studio/index.html', recomm_df=recomm_df.to_html(), today=today)
 
 if __name__ == '__main__':
     app.debug = True
-    app.run(host='127.0.0.1', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
